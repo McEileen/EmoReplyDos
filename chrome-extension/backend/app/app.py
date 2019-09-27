647289b58bf12df13479ba7ae5ffb824166ab67a
McEileen <eileenm@gmail.com>
2019-09-27 12:27:02 -0500

    extension now displays unformatted vector result

--- chrome-extension/backend/app/app.py
+++ chrome-extension/backend/app/app.py
@@ -1,13 +1,26 @@
+import os
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask import request, jsonify
 
+from emoreply_generator import find_perks_response, read_corpus, build_model, train_model
+
+source_text = os.getcwd() + "/Perks.txt"
+
 app = Flask(__name__)
 CORS(app)
+
+print("building the model....")
+untrained_model = build_model(source_text)
+print("creating the training corpus")
+train_corpus = list(read_corpus(source_text))
+print("training the model.....")
+trained_model = train_model(untrained_model, train_corpus)
+print("the model is ready!")
 
 @app.route('/responses', methods=['POST'])
 @cross_origin(allow_headers=['Content-Type'])
 def generate_emoreply():
     input = request.get_json()['input']
-    emoreply = "I got this input: " + input
+    emoreply = find_perks_response(trained_model, input, train_corpus)
     return jsonify({'response': emoreply}), 200
---
+++ chrome-extension/backend/app/emoreply_generator.py
@@ -0,0 +1,32 @@
+import gensim
+import smart_open
+
+
+def read_corpus(source_text, tokens_only=False):
+    with smart_open.open(source_text, encoding="iso-8859-1") as f:
+        for i, line in enumerate(f):
+            if tokens_only:
+                yield gensim.utils.simple_preprocess(line)
+            else:
+                yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
+
+
+def build_model(source_text):
+    corpus = list(read_corpus(source_text))
+    model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+    model.build_vocab(corpus)
+    return model
+
+
+def train_model(model, train_corpus):
+    model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+    return model
+
+
+def find_perks_response(model, input, train_corpus):
+    tokenized_input = gensim.utils.simple_preprocess(input)
+    inferred_vector = model.infer_vector(tokenized_input)
+    most_sim_vector = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))[0]
+    response_idx = most_sim_vector[0] + 1
+    response = train_corpus[response_idx]
+    return response
