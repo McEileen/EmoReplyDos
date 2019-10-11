d9e777062de53e72c39dfaae59b0a448ebb58c6f
McEileen <eileenm@gmail.com>
2019-10-11 09:38:16 -0400

    display punctuation in emoreplies

--- chrome-extension/backend/app/app.py
+++ chrome-extension/backend/app/app.py
@@ -1,20 +1,22 @@
 import os
 from flask import Flask
 from flask_cors import CORS, cross_origin
-from flask import request, jsonify
+from flask import request, Response
 
-from emoreply_generator import (find_perks_response, read_corpus,
-                                build_model, train_model)
+from emoreply_generator import (find_perks_response, create_train_corpus,
+                                create_ref_corpus, build_model, train_model)
 
 source_text = os.getcwd() + "/Perks.txt"
 
 app = Flask(__name__)
 CORS(app)
 
-print("building the model....")
-untrained_model = build_model(source_text)
 print("creating the training corpus")
-train_corpus = list(read_corpus(source_text))
+train_corpus = create_train_corpus(source_text)
+print("creating the reference corpus")
+ref_corpus = create_ref_corpus(source_text)
+print("building the model....")
+untrained_model = build_model(train_corpus)
 print("training the model.....")
 trained_model = train_model(untrained_model, train_corpus)
 print("the model is ready!")
@@ -24,5 +26,6 @@ print("the model is ready!")
 @cross_origin(allow_headers=['Content-Type'])
 def generate_emoreply():
     input = request.get_json()['input']
-    emoreply = find_perks_response(trained_model, input, train_corpus)
-    return jsonify(emoreply), 200
+    reply = find_perks_response(trained_model, input, train_corpus, ref_corpus)
+    response = Response(reply, content_type="application/json; charset=utf-8")
+    return response
--- chrome-extension/backend/app/emoreply_generator.py
+++ chrome-extension/backend/app/emoreply_generator.py
@@ -11,10 +11,22 @@ def read_corpus(source_text, tokens_only=False):
                 yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i]) # noqa
 
 
-def build_model(source_text):
+def create_train_corpus(source_text):
     corpus = list(read_corpus(source_text))
+    return corpus
+
+
+def create_ref_corpus(source_text):
+    ref_corpus = []
+    with smart_open.open(source_text) as f:
+        for i, line in enumerate(f):
+            ref_corpus.append(line)
+    return ref_corpus
+
+
+def build_model(train_corpus):
     model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40) # noqa
-    model.build_vocab(corpus)
+    model.build_vocab(train_corpus)
     return model
 
 
@@ -23,11 +35,10 @@ def train_model(model, train_corpus):
     return model
 
 
-def find_perks_response(model, input, train_corpus):
+def find_perks_response(model, input, train_corpus, ref_corpus):
     tokenized_input = gensim.utils.simple_preprocess(input)
     inferred_vector = model.infer_vector(tokenized_input)
     most_sim_vector = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))[0] # noqa
     response_idx = most_sim_vector[0]
-    response = train_corpus[response_idx]
-    reply = ' '.join(response.words).capitalize()
-    return reply
+    response = ref_corpus[response_idx].strip()
+    return response
